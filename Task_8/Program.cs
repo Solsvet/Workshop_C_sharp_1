78994e80176bf12b32361dd6fe99a5657f413111
Solsvet <mamusik47@gmail.com>
2022-07-09 21:44:40 +0300

    Fixed the program

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,11 +1,10 @@
-﻿int n = Console.ReadLine();
+﻿Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 string  answer_1 = (" - Данное число четное ");
 string  answer_2 = (" - Данное число нечетное ");
 
-Console.Write("Введите число N: ");
-
-while (count < n)
+while (count <= n)
 {
    if (count % 2 == 0) 
    {
