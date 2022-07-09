a3c0a17ea053446200db9d88752a71e4475767fd
Solsvet <mamusik47@gmail.com>
2022-07-09 20:44:23 +0300

    Added the solution of Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = Console.ReadLine();
+int count = 1;
+string  answer_1 = (" - Данное число четное ");
+string  answer_2 = (" - Данное число нечетное ");
+
+Console.Write("Введите число N: ");
+
+while (count < n)
+{
+   if (count % 2 == 0) 
+   {
+   Console.Write(count);
+   Console.WriteLine(answer_1);
+   }
+   else
+   {
+   Console.Write(count);
+   Console.WriteLine(answer_2);
+   }
+
+   count++;
+}
