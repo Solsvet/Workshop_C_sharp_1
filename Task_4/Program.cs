ab1796d33abcf1a7c95e8633a48432f6c68af4c7
Solsvet <mamusik47@gmail.com>
2022-07-09 19:38:44 +0300

    Added the solution of Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа с данными: a = 2, b = 3, c = 7
+/*int a = 2, b = 3, c = 7, max = a;
+if (a >= max) max = a;
+if (b >= max) max = b;
+if (c >= max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);*/
+
+// Программа с данными: a = 44, b = 5, c = 78
+/*int a = 44, b = 5, c = 78, max = a;
+if (a >= max) max = a;
+if (b >= max) max = b;
+if (c >= max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);*/
+
+// Программа с данными: a = 22, b = 3, c = 9
+int a = 22, b = 3, c = 9, max = a;
+if (a >= max) max = a;
+if (b >= max) max = b;
+if (c >= max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
