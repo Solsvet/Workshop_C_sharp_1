f40b26ab94102f98b4a3b3f0553e6539a0f53abb
Solsvet <mamusik47@gmail.com>
2022-07-09 20:14:37 +0300

    Added the solution of Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,36 @@
+﻿int a = 4, b = -3, c = 7;
+string  answer_1 = (" - Данное число четное ");
+string  answer_2 = (" - Данное число нечетное ");
+
+if (a % 2 == 0) 
+{
+Console.Write(a);
+Console.WriteLine(answer_1);
+}
+else
+{
+Console.Write(a);
+Console.WriteLine(answer_2);
+}
+
+if (b % 2 == 0) 
+{
+Console.Write(b);
+Console.WriteLine(answer_1);
+}
+else
+{
+Console.Write(b);
+Console.WriteLine(answer_2);
+}
+
+if (c % 2 == 0) 
+{
+Console.Write(c);
+Console.WriteLine(answer_1);
+}
+else
+{
+Console.Write(c);
+Console.WriteLine(answer_2);
+}
