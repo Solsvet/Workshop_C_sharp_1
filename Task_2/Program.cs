7a795beeb6e9a8f94abed4da13a77826b56cab01
Solsvet <mamusik47@gmail.com>
2022-07-09 19:27:50 +0300

    Added the solution of Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа с данными: a = 5, b = 7
+/*int a = 5, b = 7, max = a;
+if (a >= max) max = a;
+if (b >= max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);*/
+
+// Программа с данными: a = 2, b = 10
+/*int a = 2, b = 10, max = a;
+if (a >= max) max = a;
+if (b >= max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);*/
+
+// Программа с данными: a = -9, b = -3
+int a = -9, b = -3, max = a;
+if (a >= max) max = a;
+if (b >= max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
